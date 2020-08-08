934ea403dff331eec9e026922301bed1e2cc7767
Ivan Olmo <ivan.olmo@gmail.com>
2020-08-07 22:48:42 -0500

    Update client index file to use proper js imports

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,10 @@
-import { checkForName } from './js/nameChecker';
-import { handleSubmit } from './js/formHandler';
-
 import './styles/resets.scss';
 import './styles/base.scss';
 import './styles/footer.scss';
 import './styles/form.scss';
 import './styles/header.scss';
+import { sentiment } from './js/sentiment.js'
 
 export {
-  checkForName,
-  handleSubmit
-};
+  sentiment
+}
