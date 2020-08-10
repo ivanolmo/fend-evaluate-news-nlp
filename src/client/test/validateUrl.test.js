680726ae73e9f2a77154e0edd0046744f0ca2cf7
Ivan Olmo <ivan.olmo@gmail.com>
2020-08-09 21:00:09 -0500

    Add URL validation jest testing

---
+++ src/client/test/validateUrl.test.js
@@ -0,0 +1,13 @@
+import {validateUrl} from "..";
+
+describe('Test URL validator', () => {
+  test('Testing for valid URL input from user', () => {
+    const testInput = 'ww.thismightbeurl.';
+    expect(validateUrl(testInput) === false).toBe(true);
+  });
+  test('Testing for valid URL input from user', () => {
+    const testInput = 'www.udacity.com';
+    expect(validateUrl(testInput) === true).toBe(true);
+  });
+  }
+)
